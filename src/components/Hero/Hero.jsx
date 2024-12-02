4cb7b51d8b3c0faa95d06887f5e7d16273d2a423
BilousPM <bilouspm@gmail.com>
2024-12-02 09:22:13 -0700

    some styles for Hero components

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,9 @@ const Hero = () => {
   return (
     <section className={css.hero}>
       <h1 className={css.title}>Campers of your dreams</h1>
-      <h2>You can find everything you want in our catalog</h2>
+      <h2 className={css.subTitle}>
+        You can find everything you want in our catalog
+      </h2>
     </section>
   );
 };

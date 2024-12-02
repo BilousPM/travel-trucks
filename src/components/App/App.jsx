993e1baf53b41816fbea3a4c61a607ea92557a9b
BilousPM <bilouspm@gmail.com>
2024-12-02 11:30:06 -0700

    Adds pages and routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,19 @@
-import Header from '../Header/Header.jsx';
-import Hero from '../Hero/Hero.jsx';
+import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
+import NotFound from '../../pages/NotFound/NotFound.jsx';
+import Home from '../../pages/Home/Home.jsx';
+import Catalog from '../../pages/Catalog/Catalog.jsx';
 
 function App() {
   return (
     <>
-      <Header />
-      <Hero />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        {/* <Route path="/catalog/:id" element={} /> */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }

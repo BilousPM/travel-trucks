a53472aec1ebb5b094c3dafd496e9db8a8fb5135
BilousPM <bilouspm@gmail.com>
2024-12-04 09:41:14 -0700

    adds style for location form

--- src/components/LocationForm/LocationForm.jsx
+++ src/components/LocationForm/LocationForm.jsx
@@ -1,11 +1,28 @@
 import { Field, Form, Formik } from 'formik';
 import css from './LocationForm.module.css';
 
+// const iinitialValues = {
+//   location: 'Kyiv, Ukraine',
+// };
+
 const LocationForm = () => {
   return (
     <Formik initialValues={{}} onSubmit={() => {}}>
-      <Form className={css.input}>
-        <Field type="text" name="location" />
+      <Form>
+        <label htmlFor={location} className={css.label}>
+          Location
+        </label>
+        <Field
+          // as="select"
+          type="text"
+          name="location"
+          className={css.input}
+          id={location}
+        >
+          {/* <option value="good">Good</option>
+          <option value="neutral">Neutral</option>
+          <option value="bad">Bad</option> */}
+        </Field>
       </Form>
     </Formik>
   );

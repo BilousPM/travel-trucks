7931c3127a765f72315e889a09078d18c33fc1f2
BilousPM <bilouspm@gmail.com>
2025-02-11 13:34:49 -0700

    add reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,13 +1,25 @@
+import { selectCamperById } from '../../redux/campers/selectors.js';
 import s from './Reviews.module.css';
+import { useSelector } from 'react-redux';
 
 const Reviews = () => {
+  const camper = useSelector(selectCamperById);
+  console.log(camper.reviews);
+
   return (
     <div className={s.reviewWrapper}>
       <ul>
+        {camper.reviews.map(review => {
+          <li>
+            <div></div>
+            <h2>{review.reviewer_name}</h2>
+            <span>*</span>
+            <p>{review.coment} </p>
+          </li>;
+        })}
+
         <li>
-          <div>
-            <p></p>
-          </div>
+          <div></div>
           <h2>Name</h2>
           <span>*</span>
         </li>
